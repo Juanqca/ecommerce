bbd6e86761c38919da8cd939ecc8345a55349940
suvargas <suvargasportugal@gmail.com>
2017-06-16 06:31:46 +0000

    conroladores

---
+++ ecommerce/app/Http/Controllers/UsersController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Users;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Routing\Redirector;
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users=users::All();
+
+        return view('users.users',['users'=> $users]);
+        return view('seguridad.users',['users'=> $users]);
+        //
+    }
+
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user= users::find($id);
+        return view("users.edit",["user"=>$user]);
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id )
+    {
+        $user=Users::find($id);
+        $user->name=$request->name;
+        $user->email=$request->email;
+        $user->password=$request->password;
+        $rules=array(
+            'name'=>'required',
+            'email'=>'required',
+            'password'=>'required',
+        );
+        $this->validate($request,$rules);
+        if($user->save()){
+            return redirect("/users");
+        }else{
+            return view ("users/create",["user"=>$user]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
