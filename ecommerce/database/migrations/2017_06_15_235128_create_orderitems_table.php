4a07f9fe4bc5b7404f3cfc6b39052169080ce928
Juanqca <jctmtorrez666@gmail.com>
2017-06-15 20:30:37 -0400

    migraciones

--- ecommerce/database/migrations/2017_06_15_235128_create_orderitems_table.php
+++ ecommerce/database/migrations/2017_06_15_235128_create_orderitems_table.php
@@ -4,29 +4,32 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOrderItemsTable extends Migration
+class CreateOrderitemsTable extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-   public function up()
+    public function up()
     {
-        Schema::create('order_items', function (Blueprint $table) {
+        //
+        Schema::create('orderitems', function (Blueprint $table) {
               $table->increments('id');
             $table->decimal('price', 5, 2);
             $table->integer('quantity')->unsigned();
             $table->integer('product_id')->unsigned();
             $table->foreign('product_id')
                   ->references('id')
-                  ->on('products')
-                  ->onDelete('cascade');
+                  ->on('products');
             $table->integer('order_id')->unsigned();
             $table->foreign('order_id')
                   ->references('id')
-                  ->on('orders')
-                  ->onDelete('cascade');;
+                  ->on('orders');
+            $table->integer('provideer_id')->unsigned();
+            $table->foreign('provideer_id')
+                  ->references('id')
+                  ->on('provideers');
         });
     }
 
@@ -37,6 +40,7 @@ class CreateOrderItemsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('order_items');
+        //
+        Schema::drop('orderitems');
     }
 }
